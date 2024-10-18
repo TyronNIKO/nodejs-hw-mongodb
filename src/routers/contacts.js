07fbf2c1ab5293131f04fc06d4606420387db92f
Volodymyr Bilohorodskyi <niko.fm@gmail.com>
2024-10-18 03:31:26 +0300

    half way donw

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -15,29 +15,23 @@ import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
-router.get('/contacts', ctrlWrapper(getContactsController));
-router.get(
-  '/contacts/:contactId',
-  isValidId,
-  ctrlWrapper(getContactByIdController),
-);
+router.get('/', ctrlWrapper(getContactsController));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 router.post(
-  '/contacts',
+  '/register',
   validateBody(createContactSchema),
   ctrlWrapper(createContactController),
 );
-router.delete(
-  '/contacts/:contactId',
-  isValidId,
-  ctrlWrapper(deleteContactController),
-);
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 router.put(
-  '/contacts/:contactId',
+  '/:contactId',
+  isValidId,
   validateBody(createContactSchema),
   ctrlWrapper(upsertContactController),
 );
 router.patch(
-  '/contacts/:contactId',
+  '/:contactId',
+  isValidId,
   validateBody(createContactSchema),
   ctrlWrapper(patchContactController),
 );
--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import { env } from './utils/env.js';
 
-import contactsRouter from './routers/contacts.js';
+// import contactsRouter from './routers/contacts.js';
+import router from './routers/index.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 
@@ -16,36 +18,37 @@ const app = express();
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
-  app.use(cors());
+    app.use(
+        pino({
+            transport: {
+                target: 'pino-pretty',
+            },
+        }),
+    );
+    app.use(cors());
+    app.use(cookieParser());
 
-  app.use((req, res, next) => {
-    console.log(`Time: ${new Date().toLocaleString()}`);
-    // throw new Error('Error on home page');
-    next();
-  });
+    app.use((req, res, next) => {
+        console.log(`Time: ${new Date().toLocaleString()}`);
+        // throw new Error('Error on home page');
+        next();
+    });
 
-  app.use(express.json());
-  app.use(express.static(path.join(process.cwd(), 'public')));
-  app.get('/', (req, res) => {
-    // res.json({
-    //   message: 'Hello world!',
-    // });
-    res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
-  });
+    app.use(express.json());
+    //   app.use(express.static(path.join(process.cwd(), 'public')));
+    app.get('/', (req, res) => {
+        res.json({
+            message: 'Hello world!',
+        });
+        // res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
+    });
 
-  app.use(contactsRouter);
+    app.use(router);
 
-  app.use('*', notFoundHandler);
-  app.use(errorHandler);
+    app.use('*', notFoundHandler);
+    app.use(errorHandler);
 
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 };
