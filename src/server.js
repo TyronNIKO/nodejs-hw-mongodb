1a5f47bd6667a927e2c47391b0439c3951d88021
Vova <niko.fm@gmail.com>
2024-11-13 04:08:07 +0200

    Homework 7 done

--- src/server.js
+++ src/server.js
@@ -13,6 +13,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 
 import path from 'node:path';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const app = express();
 
@@ -47,6 +48,7 @@ export const setupServer = () => {
     app.use(router);
 
     app.use('/uploads', express.static(UPLOAD_DIR));
+    app.use('/api-docs', swaggerDocs());
 
     app.use('*', notFoundHandler);
     app.use(errorHandler);
@@ -54,5 +56,4 @@ export const setupServer = () => {
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
-    /////
 };
